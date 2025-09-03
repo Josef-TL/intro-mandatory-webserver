8d5f5874f2810cb6f02f9c0453c04015ef773a5d
Josef-TL <j.tonnesenlucas@gmail.com>
2025-09-03 10:39:42 +0200

    Started main,

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # intro-mandatory-webserver
 Mandatory exercise for Intro til IT-Sikkerhed.
+
+## Opdeling af opgaven
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from socket import *
+import andreas
+import josef
+import lykke
 
+server_port = 80
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('', server_port))
+server_socket.listen(1)
+
+print("The server is ready to receive.")
+while True:
+    connection_socket, addr = server_socket.accept()
+    msg = connection_socket.recv(2048).decode()
+
+    # Herfra skal vi bearbejde http request
+    print("Received message:", msg)
+    connection_socket.send("Response")
