ee584ea0ce9d44d23ef1b92af0637f03d9525695
Josef-TL <j.tonnesenlucas@gmail.com>
2025-09-04 09:59:48 +0200

    m2

--- josef.py
+++ josef.py
@@ -1,1 +1,8 @@
+# Jeg skal lave noget funktionalitet der laver et response
+def create_response(request_lines, status):
+    res = ["HTTP/1.1",str(status)+" OK"] 
+    res.append("asd")
+    res = " ".join(res)
+    return res
 
+print(create_response_header(2,200))
