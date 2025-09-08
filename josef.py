b81f6cb1a78071e181ac84479efd88da9e3a8fc3
Josef-TL <j.tonnesenlucas@gmail.com>
2025-09-08 10:19:15 +0200

    ammended status handling

--- josef.py
+++ josef.py
@@ -1,14 +1,16 @@
 # Jeg skal lave noget funktionalitet der laver et response
 def create_response(url, status):
     status_codes = {200: "OK",
+                    400: "Bad request",
                     404: "Not Found",
-                    500: "Internal Server Error",
-                    501: "Not Implemented"}
+                    500: "Internal Server Error"}
     
   
     # her starter response. Vi kører altid med http v1.1
     res_head = "HTTP/1.1 "
     res_body = ""
+
+    
 
     # sørger for at path er i den rigtige format.
     if url == "/":
@@ -18,29 +20,33 @@ def create_response(url, status):
     else:
         filename = url.lstrip("/") + ".html"
 
-    # Prøv at åbne filen. Hvis man kan, kør videre
-    #  hvis det fejler med 404, lav status om
-    try:
-        with open(filename, "r") as f:
-            res_body = f.read()
 
-        # Laver response header. Vi kører altid med http v1.1
-        # checker status koden og laver header derudfra.
-        
-        if status in status_codes:
+    # checker status
+    match status:
+        case 200:
+
+            # Prøv at åbne filen. Hvis man kan, kør videre
+            # hvis det fejler med 404, lav status om 
+            try:
+                with open(filename, "r") as f:
+                    res_body = f.read()
+                res_head += str(status) + " " + status_codes[status] + "\r\n"
+
+            except FileNotFoundError:
+                status = 404
+                res_body = "<html><h1>404 Not Found</h1></html>"
+                res_head += str(status) + status_codes[status] + "\r\n"
+                
+
+        case 400:
+            res_body = "<html><h1>400 Bad request</h1></html>"
             res_head += str(status) + " " + status_codes[status] + "\r\n"
-        
-        else:
-            res_head += "500 " + status_codes[500] + "\r\n"
-            status = 500
-        
-    except FileNotFoundError:
-        res_body = "<html><h1>404 Not Found</h1></html>"
-        res_head += "404 " + status_codes[404] + "\r\n"
-        status = 404
-        
 
-    
+        case 500:
+            res_body = "<html><h1>500 Internal server error</h1></html>"
+            res_head += str(status) + " " + status_codes[status] + "\r\n"
+
+
     # slutter header
     res_head += "\r\n"
 
